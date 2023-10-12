e6fdead96fa8713878498f352f005ff105b476e3
taiyebnirjhar <nirjhar.212902003@green.ac.bd>
2023-10-12 15:17:59 +0600

    update

--- Flower Logo/src/App.jsx
+++ Flower Logo/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
   const [size, setSize] = useState(15);
@@ -19,58 +19,68 @@ const App = () => {
   ];
 
   return (
-    <div
-      style={{ fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif' }}
-      className="grid place-items-center min-h-screen"
-    >
+    <div className="w-full h-screen bg-violet-600">
       <div
-        className="form-group"
-        style={{ marginBottom: "2rem", display: "grid", placeItems: "center" }}
+        style={{ fontFamily: 'Roboto, "Helvetica Neue", Arial, sans-serif' }}
+        className="flex flex-col gap-8 justify-center items-center h-screen"
       >
-        <label style={{ display: "flex", alignItems: "center" }}>
-          Size
-          <input
-            type="range"
-            id="slider"
-            min="5"
-            max="20"
-            value={size}
-            onChange={handleSizeChange}
-          />
-        </label>
-      </div>
-      <div
-        style={{
-          width: `${size}rem`,
-          height: `${size}rem`,
-          background: "#fff",
-          borderRadius: "16.667%",
-          display: "grid",
-          placeItems: "center",
-          position: "relative",
-          boxShadow: "0px 0px 10px 1px rgba(0, 0, 0, 0.2)",
-          transition: "all 0.5s ease-in-out",
-        }}
-      >
-        {Array(8)
-          .fill(null)
-          .map((_, i) => (
-            <span
-              key={i}
-              className="petal"
-              style={{
-                height: "37.5%",
-                width: "22.5%",
-                borderRadius: "50% / 30%",
-                position: "absolute",
-                transformOrigin: "center 105%",
-                transform: `rotate(${i * 45}deg)`,
-                background: bg[i],
-                top: "10%",
-                mixBlendMode: "multiply",
-              }}
-            ></span>
-          ))}
+        <div
+          className="form-group"
+          style={{
+            marginBottom: "2rem",
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <label
+            style={{ display: "flex", alignItems: "center" }}
+            className="text-white "
+          >
+            <span className="px-2"> Size</span>
+
+            <input
+              type="range"
+              id="slider"
+              min="5"
+              max="20"
+              value={size}
+              onChange={handleSizeChange}
+            />
+          </label>
+        </div>
+        <div
+          style={{
+            width: `${size}rem`,
+            height: `${size}rem`,
+            background: "#fff",
+            borderRadius: "16.667%",
+            display: "grid",
+            placeItems: "center",
+            position: "relative",
+            boxShadow: "0px 0px 10px 1px rgba(0, 0, 0, 0.2)",
+            transition: "all 0.5s ease-in-out",
+          }}
+        >
+          {Array(8)
+            .fill(null)
+            .map((_, i) => (
+              <span
+                key={i}
+                className="petal"
+                style={{
+                  height: "37.5%",
+                  width: "22.5%",
+                  borderRadius: "50% / 30%",
+                  position: "absolute",
+                  transformOrigin: "center 105%",
+                  transform: `rotate(${i * 45}deg)`,
+                  background: bg[i],
+                  top: "10%",
+                  mixBlendMode: "multiply",
+                }}
+              ></span>
+            ))}
+        </div>
       </div>
     </div>
   );
